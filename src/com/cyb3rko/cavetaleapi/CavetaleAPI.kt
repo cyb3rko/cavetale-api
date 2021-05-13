2c7cb9284530100d7dd93401897312c4a77eafda
Cyb3rKo <niko@cyb3rko.de>
2021-05-13 20:27:14 +0200

    Add method for getting profile categories

--- src/com/cyb3rko/cavetaleapi/CavetaleAPI.kt
+++ src/com/cyb3rko/cavetaleapi/CavetaleAPI.kt
@@ -88,11 +88,73 @@ class CavetaleAPI {
         return list
     }
 
+    fun getItemsSold(htmlSource: String): List<List<String>> {
+        val list = mutableListOf<List<String>>()
+        try {
+            val doc = Jsoup.parse(htmlSource)
+            val elements = doc.select("#result > tbody > *")
+            elements.forEach {
+                val sellItem = it.select(".offer").text()
+                val amount = it.select(".amount").text()
+                val price = it.select(".price").text()
+                val buyer = it.select(".playername")[1].child(0).text()
+                list.add(listOf(sellItem, amount, price, buyer))
+            }
+        } catch (e: Exception) {
+            val log = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME)
+            log.severe("Failed website parsing with exception: ${e.message}")
+        }
+        return list
+    }
+
+    fun getItemsBought(htmlSource: String): List<List<String>> {
+        val list = mutableListOf<List<String>>()
+        try {
+            val doc = Jsoup.parse(htmlSource)
+            val elements = doc.select("#result > tbody > *")
+            elements.forEach {
+                val buyItem = it.select(".offer").text()
+                val amount = it.select(".amount").text()
+                val price = it.select(".price").text()
+                val seller = it.select(".playername")[1].child(0).text()
+                list.add(listOf(buyItem, amount, price, seller))
+            }
+        } catch (e: Exception) {
+            val log = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME)
+            log.severe("Failed website parsing with exception: ${e.message}")
+        }
+        return list
+    }
+
+    fun getCurrentOffers(htmlSource: String): List<List<String>> {
+        val list = mutableListOf<List<String>>()
+        try {
+            val doc = Jsoup.parse(htmlSource)
+            val elements = doc.select("#result > tbody > *")
+            elements.forEach {
+                val sellItem = it.select(".offer").text()
+                val amount = it.select(".amount").text()
+                val moneyElements = it.select(".price").eachText().toTypedArray()
+                list.add(listOf(sellItem, amount, *moneyElements))
+            }
+        } catch (e: Exception) {
+            val log = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME)
+            log.severe("Failed website parsing with exception: ${e.message}")
+        }
+        return list
+    }
+
     fun getSearchPhrase(item: String, player: String = "", sell: Boolean = false): String {
         val processedItem = item.replace(" ", "+")
         val sellSuffix = if (sell) "&type=sell" else ""
         return "https://cavetale.com/market/?q=$processedItem&p=$player$sellSuffix"
     }
+
+    fun getItemsSoldLink(name: String) = "https://cavetale.com/market/player.php?name=$name&q=sold"
+
+    fun getItemsBoughtLink(name: String) = "https://cavetale.com/market/player.php?name=$name&q=bought"
+
+    fun getCurrentOffersLink(name: String) = "https://cavetale.com/market/player.php?name=$name&q=offers"
 
     fun getAvatarLink(name: String, size: Int) = "https://minotar.net/helm/$name/$size.png"
 
